200a8601876802b65afd69d94fc969d0bcda4481
Ramon <h_keller@gmx.net>
2020-12-29 13:23:49 +0000

    Verschiedene Dateien angelegt

--- manifests/meineVierteDateiMitOwner.pp
+++ manifests/meineVierteDateiMitOwner.pp
@@ -7,3 +7,36 @@ file { '/tmp/dateiMitOwnerUbuntu':
   ensure => present,
   owner  => 'ubuntu',
 }
+
+file { '/tmp/dateiMitGroupUbuntu':
+  ensure => present,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/tmp/dateiMitUserRechten':
+  ensure => present,
+  mode   => '0111',
+}
+
+file { '/tmp/meinVerzeichnis':
+  ensure => directory,
+  owner  => 'vagrant',
+  group  => 'ubuntu',
+  mode   => '0555',
+}
+
+file{ '/tmp/meinVerzeichnis/dateiInMeinemVerzeichnis':
+  ensure => present
+}
+
+file { '/tmp/meinRekursivesVerzeichnis':
+  source  => '/examples/files/config_dir',
+  recurse => true,
+  owner   => 'vagrant',
+}
+
+file { '/tmp/meinLinkToEtc':
+  ensure => link,
+  target => '/etc',
+}
